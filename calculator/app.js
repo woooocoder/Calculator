30fd4cef3b0c47f2d0f9f247380502de2205fe6b
woooocoder <101821974+woooocoder@users.noreply.github.com>
2023-05-12 00:44:18 -0400

    Update app.js

--- calculator/app.js
+++ calculator/app.js
@@ -6,27 +6,47 @@
     let zero = document.querySelector('.btn-zero');
     let negate = document.querySelector('.btn-negate');
 
-    // when there is a click, check which buton was pushed
+    // when there is a click, check which button was pushed
     btn.forEach(function(button) {
         // check if event (click) occurs. if event occurs, run function
         button.addEventListener('click', function(e){
             let value = e.target.dataset.num;
             screen.value += value;
-        })
+        });
     });
 
     // checking for click on "="
     equal.addEventListener('click', function(e){
         if(screen.value === '') {
-            screen.value = "";
+            screen.value = '';
         } else {
-            let answer = eval(screen.value);
+            let expression = screen.value;
+
+            // Remove trailing operators
+            while (isOperator(expression[expression.length - 1])) {
+                expression = expression.slice(0, -1);
+            }
+
+            let answer = evaluateExpression(expression);
             screen.value = answer;
         }
-    })
-
+    });
 
     clear.addEventListener('click', function(e){
-        screen.value = "";
-    })
+        screen.value = '';
+    });
+
+    function isOperator(character) {
+        return ['+', '-', '*', '/'].includes(character);
+    }
+
+    function evaluateExpression(expression) {
+        try {
+            let result = eval(expression);
+            return result;
+        } catch (error) {
+            console.error('Error evaluating expression:', error);
+            return 'Error';
+        }
+    }
 })();
