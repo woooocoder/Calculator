69e5531a09f9b95b5e20c3389d4b1922d59ee50e
woooocoder <101821974+woooocoder@users.noreply.github.com>
2023-05-22 15:53:32 -0400

    Update app.js

--- calculator/app.js
+++ calculator/app.js
@@ -1,52 +1,35 @@
-(function(){
-    let screen = document.querySelector('.screen');
-    let btn = document.querySelectorAll('.btn');
-    let clear = document.querySelector('.btn-clear');
-    let equal = document.querySelector('.btn-equal');
-    let zero = document.querySelector('.btn-zero');
-    let negate = document.querySelector('.btn-negate');
+// Get the calculator screen and buttons
+const screen = document.querySelector('.screen');
+const btn = document.querySelectorAll('.btn');
 
-    // when there is a click, check which button was pushed
-    btn.forEach(function(button) {
-        // check if event (click) occurs. if event occurs, run function
-        button.addEventListener('click', function(e){
-            let value = e.target.dataset.num;
-            screen.value += value;
-        });
-    });
-
-    // checking for click on "="
-    equal.addEventListener('click', function(e){
-        if(screen.value === '') {
+// Add event listeners to each button
+btn.forEach(button => {
+  button.addEventListener('click', () => {
+    // Get the clicked button's value
+    const value = button.dataset.num;
+    
+    // Handle button clicks
+    switch (value) {
+        case 'AC':
             screen.value = '';
-        } else {
-            let expression = screen.value;
-
-            // Remove trailing operators
-            while (isOperator(expression[expression.length - 1])) {
-                expression = expression.slice(0, -1);
-            }
-
-            let answer = evaluateExpression(expression);
-            screen.value = answer;
+            break;
+        case '+/-':
+            screen.value = parseFloat(screen.value) * -1;
+            break;
+        case '%':
+            screen.value = parseFloat(screen.value) / 100;
+            break;
+        case '=':
+            try {
+                let net = eval(screen.value);
+                screen.value = net;
+                if (screen.value.toString().length > 9) screen.value = net.toExponential(5);
+              } catch (error) {
+                screen.value = 'Error';
+              }
+            break;
+        default:
+            screen.value += value;
         }
     });
-
-    clear.addEventListener('click', function(e){
-        screen.value = '';
-    });
-
-    function isOperator(character) {
-        return ['+', '-', '*', '/'].includes(character);
-    }
-
-    function evaluateExpression(expression) {
-        try {
-            let result = eval(expression);
-            return result;
-        } catch (error) {
-            console.error('Error evaluating expression:', error);
-            return 'Error';
-        }
-    }
-})();
+});
